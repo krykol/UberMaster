266350c7663d7c5770690ff61333859c594b64e0
pawelkocwa <pkocwa@poczta.fm>
2018-11-06 19:33:27 +0100

    New name for Node class

--- venv/ListOfNodes.py
+++ venv/ListOfNodes.py
@@ -1,8 +1,22 @@
 
 # musimy się zastanowić, czy nie lepiej dodać taką klasę, miałaby ona funkcjonalność zwykłej tablicy, lecz dodatkwoo
 # sprawdzała by nam, czy przypadkiem nie dodajemy istniejącego już puntu
-
-
+import random
+from Node import Node
 class ListOfNodes():
-    def __init__(self, size, x_range, y_range):
-        
+    def __init__(self, size = 50, x_range = 100, y_range = 100):
+        x_table = []
+        y_table = []
+
+        nodes_table = []
+
+
+        while x_table.__len__() < size:
+            x_temp = random.randint(x_range)
+            y_temp = random.randint(y_range)
+
+            if x_temp in x_table and y_temp in y_table:
+                continue
+            else:
+                nodes_table.append(Node)
+
--- venv/Node.py
+++ venv/Node.py
@@ -1,4 +1,4 @@
-class Nodes:
+class Node:
     def __init__(self, x_d, y_d):
         self.x_d = x_d
         self.y_d = y_d
